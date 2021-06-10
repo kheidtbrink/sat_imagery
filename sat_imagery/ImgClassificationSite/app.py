d1f024d23686a81a054447ae2f848e4b9fd7abd1
Stephane Reis <t0bster@protonmail.com>
2021-06-10 11:28:11 +0200

    streamlit v1

---
+++ sat_imagery/ImgClassificationSite/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import requests
+import numpy as np
+from PIL import Image
+import cv2
+
+
+st.title('Image Classifier')
+st.text("Provide Url of Img for image classification")
+
